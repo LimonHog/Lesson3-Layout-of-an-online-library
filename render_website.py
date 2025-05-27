4f775a3ae6e027012db758c40c62888d5e59a2e2
LimonHog <153211989+LimonHog@users.noreply.github.com>
2025-05-27 18:38:36 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -10,13 +10,13 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 def on_reload():
 
     parser = argparse.ArgumentParser(
-        description = "Создаёт онлайн билиотеку"
+        description = 'Создаёт онлайн билиотеку'
     )
     parser.add_argument('--book_file', default='media/meta_data.json', help='задаёт нужный файл')
     args = parser.parse_args()
     
 
-    with open(args.book_file, "r", encoding="utf-8") as my_file:
+    with open(args.book_file, 'r', encoding='utf-8') as my_file:
         all_books_json = my_file.read()
 
     all_books = json.loads(all_books_json)
@@ -37,7 +37,7 @@ def on_reload():
             pages_quanity = len(books_per_page)+1
         )
         
-        with open(f'pages/index{cycle_step}.html', 'w', encoding="utf8") as file:
+        with open(f'pages/index{cycle_step}.html', 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
